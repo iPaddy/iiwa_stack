f24387c4c8181b21c1dc9a9d5337b39f3dfc0d6f
Patrick Lutz <mail@plutz.xyz>
2020-04-28 01:19:40 +0200

    added basic prediction and hitting of the puck

--- iiwa_ros/scripts/kdl_tools.cpp
+++ iiwa_ros/scripts/kdl_tools.cpp
@@ -47,7 +47,8 @@ public:
             {5,"iiwa_joint_6"},
             {6,"iiwa_joint_7"},
     };
-    state_map = { {0, "init"}, {1, "reset"}, {2, "strike"} }
+    state_map = { {0, "init"}, {1, "reset"}, {2, "strike"} };
+    count = 0;
 }
   ~IiwaKinematics(){
   }
@@ -94,10 +95,10 @@ public:
     //q_max.data(6) = -3.02; //q_min.data(6) = 3.02; //q_dot_max.data(6) = 100.0;
 
     // init of the robot position
-    KDL::Vector goal_pos(0.0, -0.9, 0.175);
-    pos_aim.p(0) = goal_pos[0];
-    pos_aim.p(1) = goal_pos[1];
-    pos_aim.p(2) = goal_pos[2];
+    KDL::Vector goal_init_pos(0.0, -0.9, 0.165);
+    pos_aim.p(0) = goal_init_pos[0];
+    pos_aim.p(1) = goal_init_pos[1];
+    pos_aim.p(2) = goal_init_pos[2];
     rot_aim.M = KDL::Rotation(0.0,0.0,1.0,
                               -0.707, -0.707, 0.0,
                               0.707,-0.707,0.0);
@@ -107,10 +108,15 @@ public:
     iiwa_pos = { 0.0,-1.53, 0.165 };
     max_reach_dist = (iiwa_pos - reach_pos).Norm();
     puck_to_goal_angle();
+    following_traj = false;
+    return true;
   }
 
   void reachedInitPos(){
-    // calculate distance between pos_aim and pos and if it is within threshold return true
+    /**
+        calculate distance between pos_aim and pos and if it is within threshold return true
+        threshold chosen by hand to be 0.01
+     */
     if(reached_init==true){
       return;
     }
@@ -123,9 +129,11 @@ public:
     if(dist_to_aim < 0.01){
       ROS_INFO_STREAM("reached init position");
       reached_init = true;
+      following_traj = false;
     }
     else{
       reached_init = false;
+      following_traj = true;
     }
   }
 
@@ -152,7 +160,7 @@ public:
         puck_pos(0)=msg.pose[i].position.x;
         puck_pos(1)=msg.pose[i].position.y;
         puck_pos(2)=msg.pose[i].position.z;
-        //ROS_INFO_STREAM("Puck position: x:" << puck_pos(0));
+        //ROS_INFO_STREAM("Puck position: y:" << puck_pos(1));
       }
     }
     subscribed_puck = true;
@@ -202,14 +210,15 @@ public:
   }
 
   void puck_to_goal_angle(){
-    // calculates the angle to hit the puck for it to got towards the opponents goal
-    // angle is 0 if puck has to be hit straight forward (along y axis)
+    /**
+        calculates the angle to hit the puck for it to got towards the opponents goal
+        angle is 0 if puck has to be hit straight forward (along y axis)
+     */
+    KDL::Vector goal_pos(0.0, 1.0, 0.175);
     KDL::Vector diff = goal_pos - puck_pos;
     //ROS_INFO_STREAM("Goal - puck: " << goal_pos - puck_pos );
 
-    // TODO look into angle getting too big - should max out going towards PI/2
-    // getting angle between y axis and point
-    goal_angle = atan2(diff[0],diff[1]);
+    goal_angle = atan2(diff[1],diff[0]);
     //ROS_INFO_STREAM("Goal angle: " << goal_angle );
 
   }
@@ -217,17 +226,19 @@ public:
   void direct_to_puck_traj(){
     //calculate intermediate points between two given points
     // given 1 second 50 points between the current ee position and the puck are calculated
-    float time = 1; // secs
+    float time = 0.5; // secs
     int steps = int(time/timestep);
     // get distance from start to puck and divide by number of steps for each increase - linear interpolation
-    KDL::Vector diff = puck_pos - pos.p;
+    //TODO substitute puck_pos with puck_pred
+    //KDL::Vector diff = puck_pos - pos.p;
+    KDL::Vector diff = puck_prediction() - pos.p;
     KDL::Vector start_pos = pos.p;
     float incr_x = diff[0] / steps;
     float incr_y = diff[1] / steps;
-    ROS_INFO_STREAM("starting trajectory pushing, with steps: " << steps);
+    //ROS_INFO_STREAM("starting trajectory pushing, with steps: " << steps);
     for(int i = 0; i < steps; i++){
       traj.push_back(KDL::Vector(start_pos[0] + incr_x*i, start_pos[1] + incr_y*i, 0.175 ));
-      ROS_INFO_STREAM("pushed trajectory point: x: " << start_pos[0] + incr_x * i << " y: " << start_pos[1] + incr_y * i);
+      //ROS_INFO_STREAM("pushed trajectory point: x: " << start_pos[0] + incr_x * i << " y: " << start_pos[1] + incr_y * i);
     }
   }
   //void update_velocities(){
@@ -244,9 +255,9 @@ public:
     // TODO get current velocity for striker and puck
     // TODO parametrize 50 for frequency
     int steps = int(duration/50);
-
-    KDL::Vector diff = puck_pos - pos.p;
-      //traj.push_back(KDL::Vector(diff[0] + incr * i, diff[1] + incr * i, 0.175));
+  
+  KDL::Vector diff = puck_pos - pos.p;
+    ////traj.push_back(KDL::Vector(diff[0] + incr * i, diff[1] + incr * i, 0.175));
   }
 
   void follow_traj_point(){
@@ -259,10 +270,12 @@ public:
   }
 
   void hitPuck(){
+    // TODO write in a way that it takes for example 50 steps and after it is done sets following_traj to false
     // check if puck is in reachable area
-    if((puck_pos - iiwa_pos).Norm() < max_reach_dist){
+    //if((puck_pos - iiwa_pos).Norm() < max_reach_dist){
+    if((puck_prediction() - iiwa_pos).Norm() < max_reach_dist){
       // write hitting here
-      ROS_INFO_STREAM("Puck reachable: " << (puck_pos - iiwa_pos).Norm());
+      //ROS_INFO_STREAM("Puck reachable: " << (puck_pos - iiwa_pos).Norm());
       //calc_traj(puck_pos);
 
       // follow basic interpolated trajectory
@@ -270,13 +283,14 @@ public:
         direct_to_puck_traj();
       }
       follow_traj_point();
-      ROS_INFO_STREAM("number of trajectory points left: " << traj.size());
+      //ROS_INFO_STREAM("number of trajectory points left: " << traj.size());
 
-      // TODO use goal_angle
+      // TODO use goal_angle for an advanced tactic, but basic hitting sufices for normal gameplay
       // TODO set velocity for hitting
     }
     else{
-      ROS_INFO_STREAM("Puck is not reachable with robot");
+      //ROS_INFO_STREAM("Puck is not reachable with robot");
+      following_traj = false;
       return;
     }
   }
@@ -286,19 +300,26 @@ public:
   }
 
   void updateAimPos(){
-    // update the current aimed for position - overwriting the rotation to be fixed
-    // TODO check if rotational freedom of yaw can stay variable
+    /**
+        update the current aimed for position - overwriting the rotation to be fixed
+        TODO check if rotational freedom of yaw can stay variable
+     */
     pos_aim.M = rot_aim.M;
 
     // inverse kinematics solver giving the corresponding joint angles for a given cartesian position
     ikSolver();
 
+    // update the joint positions with the calculated positions from inverse kinematics
+    // conversion from KDL::JntArray to Float64MultiArray for sending ros messages
     joints.data.resize(7);
     joints.data = {q_aim.data[0], q_aim.data[1], q_aim.data[2], q_aim.data[3], q_aim.data[4], q_aim.data[5], q_aim.data[6]};
 
   }
 
   void ikSolver(){
+    /**
+       Inverse kinematics solver giving the matching joint angles for a cartesian point 
+     */
     iksolver = new KDL::ChainIkSolverPos_LMA(chain);
     inv_kinematics_status = iksolver->CartToJnt(q_cur,pos_aim,q_aim);
     if(inv_kinematics_status>=0){
@@ -308,7 +329,10 @@ public:
   }
 
   void fkSolver(){
-    // create solver based on kinematic tree
+    /**
+       translating joint angles to cartesian positions given the kinematic chain
+       writes position values to pos
+     */
     fksolver = new KDL::ChainFkSolverPos_recursive(chain);
 
     kinematics_status = fksolver->JntToCart(q_cur,pos);
@@ -328,29 +352,66 @@ public:
     }
   }
   void updateParams(){
-    // save a velocity value by storing previous position
+    /**
+       update the puck and endefector velocity values by storing previous position and doing simple derivation
+       also storing the current positions as the previous one for the next call of this function
+    */
     puck_vel = (puck_pos - prev_puck_pos)/timestep;
+    ROS_INFO_STREAM("puck velocity: " << puck_vel);
     ee_vel = (pos.p - prev_ee_pos)/timestep;
     prev_ee_pos = pos.p;
     prev_puck_pos = puck_pos;
-    //prev_ee_pos = ;
+  }
 
+  KDL::Vector puck_prediction(){
+    //predict the position of the puck for a given amount of time
+    KDL::Vector prediction = puck_pos + (timestep * puck_vel * 2.5);
+    // x=[-0.43,0.43] y=[-0.9,0.9] z=0.11
+    float x_max = 0.43;
+    float x_min = -0.43;
+    float y_max = 0.9;
+    float y_min = -0.9;
+
+    if(prediction[0] > x_max){
+      prediction[0] = 2*x_max - prediction[0];
+    }
+    if(prediction[0] < x_min){
+      prediction[0] = 2*x_min + prediction[0];
+    }
+    if(prediction[1] > y_max){
+      prediction[1] = 2*y_max - prediction[1];
+    }
+    if(prediction[1] < y_min){
+      prediction[1] = 2*y_min + prediction[1];
+    }
+    ROS_INFO_STREAM("current position: " << puck_pos);
+    ROS_INFO_STREAM("prediction  puck: " << prediction);
+
+    //TODO implement bounce - where are the borders, if it will meet one reverse the corresponding direction on impact
+    return prediction;
   }
 
   void mainCallback(){
     // time events
+    // used for debugging to see if a task like inverse kinematics takes too long
     using std::chrono::duration_cast;
     using std::chrono::nanoseconds;
     typedef std::chrono::high_resolution_clock clock;
     auto start = clock::now();
 
-    // update velocities
-    updateParams();
+    // update velocities every 10th iteration
+    if (!(count % 10)){
+      updateParams();
+    }
+    count++;
+
     // update cartesian position given the joint angles
     fkSolver();
 
-    // update the puck angles
+    // update the angle for the puck to go into the goal
+    // oputputting radiant angle right now
     puck_to_goal_angle();
+    //puck_prediction();
 
     // check if initialization has completed
     if(reached_init == false){
@@ -360,13 +421,20 @@ public:
     // main loop for getting a new aim and updating accordingly
     if(reached_init==true && reachedAim() ){
       //ROS_INFO_STREAM("moving to next aim");
-      // TODO some procedure here for getting the next command
+      // TODO some procedure here for getting the next command but has to be async for not blocking
       //assignAimPos();
-      //updateAimPos();
-    }
-    // set trajectory and push to next aim
-    hitPuck();
 
+      //updateAimPos();
+
+    }
+
+    //ROS_INFO_STREAM("currently following a trajectory?: " << following_traj);
+    // if not currently following a trajectory start one
+    if(not following_traj){
+      // set trajectory and push to next aim
+      //ROS_INFO_STREAM("Following trajectory");
+      hitPuck();
+    }
     // publish joint angles to robot
     pub.publish(joints);
     auto end = clock::now();
@@ -374,9 +442,6 @@ public:
     // assuming a maximum duration of about 600000 ns leaving room for the 0.02 sec > 2,000,000ns 
     if (2000000 < duration_cast<nanoseconds>(end - start).count()){
         ROS_INFO_STREAM("taking to long for 50Hz");
-      }
-      else{
-        ROS_INFO_STREAM("taking: " << duration_cast<nanoseconds>(end - start).count() << "ns/n thus trying another step");
       }
   }
 
@@ -396,16 +461,17 @@ private:
   KDL::ChainFkSolverPos_recursive *fksolver;
   KDL::JntArray q_dot_max, q_dot_aim; // maximum joint velocity
   KDL::Frame pos, pos_aim, rot_aim; // Create the frame that will contain the results
-  KDL::Vector goal_pos, puck_pos, reach_pos, iiwa_pos, ee_vel, puck_vel; // endefector positions for both robots
+  KDL::Vector goal_init_pos, goal_pos, puck_pos, reach_pos, iiwa_pos, ee_vel, puck_vel; // endefector positions for both robots
   double max_reach_dist;
   bool kinematics_status, inv_kinematics_status, reached_init, vel_status;
   KDL::Twist vel_full_aim;
   KDL::Vector vel_aim;
   float goal_angle;
   std::vector<KDL::Vector> traj; // array for positions to go to
+  uint count;
 public:
   int steps;
-  bool subscribed_puck,subscribed_joints;
+  bool subscribed_puck,subscribed_joints,following_traj;
   KDL::Vector prev_puck_pos, prev_ee_pos;
   float timestep;
 
@@ -416,6 +482,7 @@ int main(int argc, char **argv){
   ros::init(argc, argv, "kinematics");
   IiwaKinematics IiwaKinematics;
 
+  // sets the frequency to 50Hz
   float frequency = 50.;
   ros::Rate r(frequency);
   std::string robotParam = "robot_description";
